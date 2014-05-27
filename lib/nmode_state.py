48fd307366c58a9a4bca7d4443818772ea00a82b
Reed Essick <ressick@mit.edu>
2014-05-27 11:45:22 -0400

    fixed a bug in threemode_equilib concerning the daughter amplitudes. I needed to take some absolute values where I wasn't

--- lib/nmode_state.py
+++ lib/nmode_state.py
@@ -178,7 +178,7 @@ def threeMode_equilib(triple, freq, network, verbose=False):
   Ao2 = (yi*yj + di*dj)/(wi*wj*4*k**2) # = yi*yj/(4*k**2*wi*wj) * ( 1 + (didj/yiyj)**2 )
   AL2 =  ( wo*Uo )**2 / ( do**2 + yo**2 ) 
 
-  Ai2 = (yj/-wj) * (yo*yiyj + do*didj) / (4*k**2*wo*yiyj) * ( 1 + ( 1 + (4*k**2*wi*wj*(do**2+yo**2)*yiyj**2)/(yi*yj*(yo*yiyj + do*didj)**2)*(AL2 - Ao2)   )**0.5 )
+  Ai2 = (yj/-wj) * (yo*yiyj + do*abs(didj)) / (4*k**2*wo*yiyj) * ( 1 + ( 1 + (4*k**2*wi*wj*(do**2+yo**2)*yiyj**2)/(yi*yj*(yo*yiyj + do*abs(didj))**2)*(AL2 - Ao2)   )**0.5 )
   Aj2 = (wj*yi)/(wi*yj) * Ai2
 
   Ao = Ao2**0.5
